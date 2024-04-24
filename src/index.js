e54371af49b3c0898e9eb3f06aa77674291fa68f
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-24 18:22:47 +0530

    imported configure store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider} from 'react-redux'
 import App from './App';
-
+ import {ConfigureStore} from '@reduxjs/toolkit'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
