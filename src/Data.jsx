be29271ff79333bfe9100dca9325f0f992ce3966
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-25 20:24:28 +0530

    defined an array called userlist with objects like name,email,id

---
+++ src/Data.jsx
@@ -0,0 +1,17 @@
+export const userList=[
+    {
+        name : "asna",
+        email : "asnahamza333@gmail.com",
+        id:1
+    },
+    {
+        name : "hanna",
+        email : "hannahamza333@gmail.com",
+        id:2
+    },
+    {
+        name : "haneen",
+        email : "haneenhamza333@gmail.com",
+        id:3
+    }
+]
