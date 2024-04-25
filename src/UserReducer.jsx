bdde01f8f06376a02529e1947bb8858f9615f211
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-25 20:33:06 +0530

    initialized userlist

--- src/UserReducer.jsx
+++ src/UserReducer.jsx
@@ -1,10 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { userList } from "./Data";
 
 const userSlice = createSlice({
     name :"users",
-    initialState :{ },
+    initialState :userList ,
     reducers:{
-        
+
 
     }
 
