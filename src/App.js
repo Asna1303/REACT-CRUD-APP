4470f62524ea744194f79c221c514121c03ef29b
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-26 18:37:03 +0530

    mporetd browser router,routes,route from react dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from './Home'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter , Routes, Route} from 'react-router-dom'
 
 
 function App() {
