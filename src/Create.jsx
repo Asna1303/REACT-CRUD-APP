3849275cc26aace19844f0a89e719d0830db880b
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-26 21:06:00 +0530

    Added state management for name and email fields in Create component

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Create() {
+    const [name,setName] = useState('')
+    const [email,setEmail] = useState('')
+
   return (
     <div className='d-flex w-100 vh-100 justify-content-center align-items-center'>
       <div className='w-50 border bg-secondary text-white p-5'>
@@ -8,11 +11,11 @@ function Create() {
         <form>
           <div>
             <label htmlFor="name">Name: </label>
-           < input type="text" name='name' className='form-control' />
+           < input type="text" name='name' className='form-control' placeholder='enter name' />
           </div>
           <div>
             <label htmlFor="email">Email:</label>
-            <input type="email" name='email' className="form-control" />
+            <input type="email" name='email' className="form-control" placeholder='enter email id'/>
           </div><br />
           <button className='btn btn-info'>Submit</button>
         </form>
