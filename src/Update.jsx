27b65a1bf96287c12b2904eb7e0d99e94151fabd
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-28 20:41:00 +0530

    defined useparams hook to access id

--- src/Update.jsx
+++ src/Update.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
 const Update = () => {
+    const id=useParams();
   return (
-    <div>
-      
+    <div className='d-flex w-100 vh-100 justify-content-center align-items-center'>
+    <div className='w-50 border bg-secondary text-white p-5'>
+      <h3>Update user</h3>
+      <form>
+        <div>
+          <label htmlFor="name">Name: </label>
+         < input type="text" name='name' className='form-control' placeholder='enter name'/>
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" name='email' className="form-control" placeholder='enter email id'/>
+        </div><br />
+        <button className='btn btn-info'>Update</button>
+      </form>
     </div>
+  </div>
   )
 }
 
