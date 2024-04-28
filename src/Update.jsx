8dddba8430c3b96f3366e7bad78366f294618f5e
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-28 20:43:49 +0530

    defined useSelector

--- src/Update.jsx
+++ src/Update.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 const Update = () => {
-    const id=useParams();
+    const{id} =useParams();
+    const users = useSelector((state)=> state.users);
   return (
     <div className='d-flex w-100 vh-100 justify-content-center align-items-center'>
     <div className='w-50 border bg-secondary text-white p-5'>
