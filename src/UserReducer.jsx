fa882ee3326c6e5195c7d9be7e2317bfacc49f6d
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-30 23:35:04 +0530

    added update user function in reducer

--- src/UserReducer.jsx
+++ src/UserReducer.jsx
@@ -7,6 +7,9 @@ const userSlice = createSlice({
     reducers:{
         addUser: (state,action)=> {
            state.push(action.payload);
+        },
+        updateUser:(state,action)=>{
+            
         }
 
 
